a04d58d8eda9c3096afcb7628298bd7a27bbdb04
Oscar A. Konno Sampaio <oscarks@gmail.com>
2013-10-04 23:28:34 -0300

    version 0.4.3

--- grails-app/taglib/org/grails/plugins/springmobile/SpringMobileTagLib.groovy
+++ grails-app/taglib/org/grails/plugins/springmobile/SpringMobileTagLib.groovy
@@ -1,16 +1,30 @@
 package org.grails.plugins.springmobile
 
 class SpringMobileTagLib {
+
+	static namespace = "sm"
+	
 	def ifMobileDevice = { attrs, body ->
 		def device = request.getAttribute("currentDevice")
-		if(device.isMobile()){
+		if(device && device.isMobile()){
 			out << body()
 		}
 	}
 	def ifNotMobileDevice = { attrs, body ->
 		def device = request.getAttribute("currentDevice")
-		if(!device.isMobile()){
+		if(!device || !device.isMobile()){
 			out << body()
 		}
 	}
+	
+	// Auto-adapt mobile tags
+	def actionSubmit = {attrs, body ->
+		if(isMobileDevice()) {
+			
+		} else {
+			out << g.actionSubmit(attrs,body)
+		}
+	}
+
+	
 }
