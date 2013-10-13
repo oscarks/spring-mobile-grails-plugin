4525177ca1966a888d2f1734c879b826619559f1
Oscar A. Konno Sampaio <oscarks@gmail.com>
2013-10-13 02:08:11 -0300

    Implement action submit adapt tag

--- grails-app/taglib/org/grails/plugins/springmobile/SpringMobileTagLib.groovy
+++ grails-app/taglib/org/grails/plugins/springmobile/SpringMobileTagLib.groovy
@@ -7,9 +7,17 @@ class SpringMobileTagLib {
 	def ifMobileDevice = { attrs, body ->
 		def device = request.getAttribute("currentDevice")
 		if(device && device.isMobile()){
+			pageScope.doElseNotMobile=false
 			out << body()
+		} else {
+			pageScope.ifMobileElse=true
 		}
 	}
+	
+	def elseNotMobile = { attrs, body ->
+		if(pageScope.doElseNotMobile)  out << body()
+	} 
+	
 	def ifNotMobileDevice = { attrs, body ->
 		def device = request.getAttribute("currentDevice")
 		if(!device || !device.isMobile()){
@@ -20,11 +28,23 @@ class SpringMobileTagLib {
 	// Auto-adapt mobile tags
 	def actionSubmit = {attrs, body ->
 		if(isMobileDevice()) {
-			
+			def action	= attrs.action
+			def value	= attrs.value
+			def name	= attrs.name
+			out << "<input type=\"hidden\" name=\"_action_$action\" value=\"${value}\"/>"
+			def pars=""
+			attrs.findAll { !(it.key in ['action','value'])}.each {k,v->
+				pars+=" $k=\"v\" "
+			}
+			if (name && !("id" in attrs.keySet()))
+				pars+= " id=\"${name}\" "
+			out << "<button $pars type=\"submit\">${value}</button>"
 		} else {
 			out << g.actionSubmit(attrs,body)
 		}
 	}
+	
+	
 
 	
 }
