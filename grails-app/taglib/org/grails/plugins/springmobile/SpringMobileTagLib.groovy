7fe729fe749f433770b7649ef73ba7f673a167c9
Oscar A. Konno Sampaio <oscarks@gmail.com>
2013-10-14 00:45:09 -0300

    Added adapt datepicker tag lib, with jquery-ui integration

--- grails-app/taglib/org/grails/plugins/springmobile/SpringMobileTagLib.groovy
+++ grails-app/taglib/org/grails/plugins/springmobile/SpringMobileTagLib.groovy
@@ -25,26 +25,4 @@ class SpringMobileTagLib {
 		}
 	}
 	
-	// Auto-adapt mobile tags
-	def actionSubmit = {attrs, body ->
-		if(isMobileDevice()) {
-			def action	= attrs.action
-			def value	= attrs.value
-			def name	= attrs.name
-			out << "<input type=\"hidden\" name=\"_action_$action\" value=\"${value}\"/>"
-			def pars=""
-			attrs.findAll { !(it.key in ['action','value'])}.each {k,v->
-				pars+=" $k=\"v\" "
-			}
-			if (name && !("id" in attrs.keySet()))
-				pars+= " id=\"${name}\" "
-			out << "<button $pars type=\"submit\">${value}</button>"
-		} else {
-			out << g.actionSubmit(attrs,body)
-		}
-	}
-	
-	
-
-	
 }
